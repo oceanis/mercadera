0c9555d0b31a597262e336849b7819d822d21a2c
Sergio Tapia <sergiotapia@outlook.com>
2015-03-29 13:18:29 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'https://rubygems.org'
+
+ruby '2.2.1'
+
+gem 'rails', '4.2.1'
+gem 'pg'
+gem 'passenger'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.0'
+gem 'responders', '~> 2.0'
+gem 'active_model_serializers'
+gem 'aws-sdk', '~> 1.63.0'
+gem 'rails_admin'
+gem 'slim-rails'
+gem 'twitter-bootstrap-rails'
+
+group :development, :test do
+  gem 'spring'
+  gem 'rspec-rails', '~> 3.1.0'
+  gem 'factory_girl_rails', '~> 4.4.1'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+group :test do
+  gem "faker", "~> 1.4.3"
+  gem "capybara", "~> 2.4.3"
+  gem "database_cleaner", "~> 1.3.0"
+  gem "launchy", "~> 2.4.2"
+  gem "selenium-webdriver", "~> 2.43.0"
+end
---
+++ db/schema.rb
@@ -0,0 +1,93 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150328231851) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorited_listings", force: :cascade do |t|
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorited_listings", ["listing_id"], name: "index_favorited_listings_on_listing_id", using: :btree
+  add_index "favorited_listings", ["user_id"], name: "index_favorited_listings_on_user_id", using: :btree
+
+  create_table "listing_pictures", force: :cascade do |t|
+    t.string  "url"
+    t.integer "listing_id"
+  end
+
+  add_index "listing_pictures", ["listing_id"], name: "index_listing_pictures_on_listing_id", using: :btree
+
+  create_table "listings", force: :cascade do |t|
+    t.integer  "subcategory_id"
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price",          precision: 8, scale: 2
+    t.string   "contact_name"
+    t.string   "email"
+    t.string   "telephone"
+    t.integer  "location_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.datetime "expires_at"
+  end
+
+  add_index "listings", ["location_id"], name: "index_listings_on_location_id", using: :btree
+  add_index "listings", ["subcategory_id"], name: "index_listings_on_subcategory_id", using: :btree
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reported_listings", force: :cascade do |t|
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reported_listings", ["listing_id"], name: "index_reported_listings_on_listing_id", using: :btree
+  add_index "reported_listings", ["user_id"], name: "index_reported_listings_on_user_id", using: :btree
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subcategories", ["category_id"], name: "index_subcategories_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "api_token"
+    t.string   "name"
+    t.string   "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
