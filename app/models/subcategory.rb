5ad33fcddc10d6285d23b318c76928c9034c3ff2
Sergio Tapia <sergiotapia@outlook.com>
2015-03-29 14:02:07 -0400

    Remove unused Subcategory model.

--- app/models/subcategory.rb
+++
@@ -1,8 +0,0 @@
-class Subcategory < ActiveRecord::Base
-  belongs_to :category
-  has_many :listings
-
-  validates :name, presence: true
-  validates :name, uniqueness: true
-  validates :category_id, presence: true
-end
